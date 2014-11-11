19d7ac4b91f8c88334209411e1acf41d2fb4d9e7
Sergio Mironescu <sergiu.mironescu@gmail.com>
2014-11-11 18:47:11 +0100

    Added total_hours

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,4 +5,14 @@ class Project < ActiveRecord::Base
     order(created_at: :desc).limit(number)
   end
 
+  def total_hours
+    hours = 0
+    minutes = 0
+    entries.each do |entry|
+      hours += entry.hours
+      minutes += entry.minutes
+    end
+    hours + (minutes / 60)
+  end
+
 end
