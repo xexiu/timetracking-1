43fa9daa2d820bb7877abdffb022ee5803dd6606
Sergio Mironescu <sergiu.mironescu@gmail.com>
2014-11-11 13:58:09 +0100

    Added no projects .. params

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,15 @@
 class ProjectsController < ApplicationController
   def index
     @projects = Project.last_created_projects(10)
+    render 'no_projects' if @projects.empty?
   end
+
+  def show
+    unless @project = Project.find_by(id: params[:id])
+    #        = /pojects/:id
+    render 'no_projects_found'
+    # /no_projects_found.html.erb
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get '/' => 'site#home'
   get '/contact' => 'site#contact'
   get '/projects' => 'projects#index'
+  get '/projects/:id' => 'projects#show'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
