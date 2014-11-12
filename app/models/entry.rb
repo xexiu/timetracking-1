942c97dda52f821cd3431088adf4e989b1bb08b9
Sergio Mironescu <sergiu.mironescu@gmail.com>
2014-11-12 11:26:58 +0100

    Validation for entries if project doesnt not exist

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -2,5 +2,14 @@ class Entry < ActiveRecord::Base
   belongs_to :project
   validates :hours, :minutes, numericality: true, presence: true
   validates :date, presence: true
-  validates :project, presence: true
+  validates :project_id, presence: true
+
+  validate :project_exists
+  private
+  def project_exists
+    unless Project.find_by(id: project_id) # bug with friendlyid
+      errors.add(:project_id, 'does no exist')
+    end
+  end
+
 end
