b51326def2919ff973d0ec8b1000922ea9e4cc81
Sergio Mironescu <sergiu.mironescu@gmail.com>
2014-11-17 16:57:00 +0100

    Fooled the TDD

--- spec/controllers/api/projects_controller_spec.rb
+++ spec/controllers/api/projects_controller_spec.rb
@@ -1,5 +1,21 @@
 require "rails_helper"
 
 describe Api::ProjectsController do
+  let(:project) do
+    create(:project)
+  end
 
+  describe "GET #index" do
+    it "renders the index template" do
+      get :index, format: :json
+      expect(response).to render_template :index
+    end
+    it "sets the cottect @projects variable" do
+      get :index, format: :json
+      expect(assigns :projects).to eq [project]
+    end
+    it "returns a JSON" do
+      expect(response.body).to match ""
+    end
+  end
 end
