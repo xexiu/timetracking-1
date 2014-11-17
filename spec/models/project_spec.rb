69c25e5a156ca3baa7c2140ff989ef0369a68ee8
Sergio Mironescu <sergiu.mironescu@gmail.com>
2014-11-17 14:43:26 +0100

    5 tests for model project

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -14,13 +14,23 @@ describe 'Project' do
       expect(project).not_to be_valid
     end
     context 'when the name is longer than 30 chars' do
-      it 'is not valid'
+      it 'is not valid' do
+        project.name = "a" * 31
+        expect(project).not_to be_valid
+      end
     end
     context 'when the name is allready taken' do
-      it 'is not valid'
+      it 'is not valid' do
+        Project.create(name: 'Project1')
+        project.name = 'Project1'
+        expect(project).not_to be_valid
+      end
     end
     context 'when the name has special char' do
-      it 'is not valid'
+      it 'is not valid' do
+        project.name = "´"
+        expect(project).not_to be_valid
+      end
     end
   end
 end
